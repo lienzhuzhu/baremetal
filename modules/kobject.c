8c9f807c40a8162b1160bc5c0f8563df8e811eeb
Lien Zhu <lienzhucs@gmail.com>
2023-01-28 23:47:10 -0600

    about to change file data from int to char

--- modules/kobject.c
+++ modules/kobject.c
@@ -4,24 +4,23 @@
 #include <linux/module.h>
 #include <linux/init.h>
 
-static int foo;
+static int led_state;
+
+#define MAX_SIZE 1024
+#define BCM2837_GPIO_ADDRESS 0x3f200000
 
 
-static ssize_t foo_show(struct kobject *kobj, struct kobj_attribute *attr,
-			char *buf)
-{
-	return sprintf(buf, "%d\n", foo);
+static ssize_t led_state_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf) {
+	return sprintf(buf, "%d\n", led_state);
 }
 
-static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr,
-			 const char *buf, size_t count)
-{
+static ssize_t led_state_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count) {
 	int var, ret;
 
 	ret = kstrtoint(buf, 10, &var);
-	printk(KERN_DEBUG "foo update: %d -> %d\n", foo, var);
+	printk(KERN_DEBUG "led_state update: %d -> %d\n", led_state, var);
 
-	ret = kstrtoint(buf, 10, &foo);
+	ret = kstrtoint(buf, 10, &led_state);
 
 	if (ret < 0)
 		return ret;
@@ -29,20 +28,19 @@ static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr,
 	return count;
 }
 
-static struct kobj_attribute foo_attribute = __ATTR(foo, 0664, foo_show, foo_store);
+static struct kobj_attribute led_state_attribute = __ATTR(led_state, 0664, led_state_show, led_state_store);
 
 static struct kobject *led_dir;
 
-static int __init example_init(void)
-{
+static int __init led_driver_init(void) {
 	int retval;
 
-	led_dir = kobject_create_and_add("kobject_example", kernel_kobj);
+	led_dir = kobject_create_and_add("led_dir", kernel_kobj);
 	if (!led_dir) {
 		return -ENOMEM;
 	}
 
-	retval = sysfs_create_file(led_dir, &foo_attribute.attr);
+	retval = sysfs_create_file(led_dir, &led_state_attribute.attr);
 	if (retval) {
 		kobject_put(led_dir);
 	}
@@ -50,11 +48,10 @@ static int __init example_init(void)
 	return retval;
 }
 
-static void __exit example_exit(void)
-{
+static void __exit led_driver_exit(void) {
 	kobject_put(led_dir);
 }
 
-module_init(example_init);
-module_exit(example_exit);
+module_init(led_driver_init);
+module_exit(led_driver_exit);
 MODULE_LICENSE("GPL v2");
